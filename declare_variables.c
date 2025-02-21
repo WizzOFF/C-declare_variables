256e32d791bc3086827c00a5d11c6e3a49adcdac
WizzOFF <maher_off@proton.me>
2025-02-21 15:52:38 +0100

    declare_variables.c: print the contents of a variable

--- declare_variables.c
+++ declare_variables.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+/* 
 int main(int argc, char *argv[]) // Équivalent de int main()
 {
   int nombreDeVies;
   nombreDeVies = 5;
   nombreDeVies = 4;
   nombreDeVies = 3;
-     
+}
+*/
+
+int main(int argc, char *argv[])
+{
+  int nombreDeVies = 5; // Au départ, le joueur a 5 vies
+  
+  printf("Vous avez %d vies\n", nombreDeVies);
+  printf("**** B A M ****\n"); // Là il se prend un grand coup sur la tête
+  nombreDeVies = 4; // Il vient de perdre une vie !
+  printf("Ah desole, il ne vous reste plus que %d vies maintenant !\n\n", nombreDeVies);
   return 0;
 }
