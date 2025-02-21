d9e9d9fe9d4cad83ffd14b99d7f22a8bc881c973
WizzOFF <maher_off@proton.me>
2025-02-21 16:33:31 +0100

    declare_variables.c: recover a seizure

--- declare_variables.c
+++ declare_variables.c
@@ -18,13 +18,22 @@ int main(int argc, char *argv[])
   nombreDeVies = 4; // Il vient de perdre une vie !
   printf("Ah desole, il ne vous reste plus que %d vies maintenant !\n\n", nombreDeVies);
 }
-*/
 
 int main(int argc, char *argv[])
 {
   int nombreDeVies = 5, niveau = 1;
   
   printf("Vous avez %d vies et vous etes au niveau n° %d\n", nombreDeVies, niveau);
+}
+*/
+
+int main(int argc, char *argv[])
+{
+  int age = 0; // On initialise la variable à 0
   
+  printf("Quel age avez-vous ? ");
+  scanf("%d", &age); // On demande d'entrer l'âge avec scanf
+  printf("Ah ! Vous avez donc %d ans !\n\n", age);
+
   return 0;
 }
